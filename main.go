1c26f0a58b9ff5c300f1f71b47866a5d5718f9cb
mtlbtc <Blagovest.Tchasovnikarov@sas.com>
2019-02-15 09:16:01 +0000

    Added support for tracking the game state

--- main.go
+++ main.go
@@ -10,6 +10,14 @@ import (
 
 type Hand []deck.Card
 
+type State uint8
+
+const (
+	PlayerTurn State = iota
+	DealerTurn
+	HandOver
+)
+
 func (h Hand) String() string {
 	strs := make([]string, len(h))
 	for i := range h {
@@ -24,6 +32,37 @@ func (h Hand) DealerString() string {
 
 func draw(cardDeck []deck.Card) (deck.Card, []deck.Card){
 	return cardDeck[0], cardDeck[1:]
+}
+
+type GameState struct {
+	Deck []deck.Card
+	State State
+	Player Hand
+	Dealer Hand
+}
+
+func (gs *GameState) CurrentPlayer() *Hand {
+	switch gs.State {
+	case PlayerTurn:
+		return &gs.Player
+	case DealerTurn:
+		return &gs.Dealer
+	default:
+		panic("It isn't anyone's turn yet")
+	}
+}
+
+func clone(gs GameState) GameState {
+	clonedGs := GameState {
+		Deck: make([]deck.Card, len(gs.Deck)),
+		State: gs.State,
+		Player: make(Hand, len(gs.Player)),
+		Dealer: make(Hand, len(gs.Dealer)),
+	}
+	copy(clonedGs.Deck, gs.Deck)
+	copy(clonedGs.Player, gs.Player)
+	copy(clonedGs.Dealer, gs.Dealer)
+	return clonedGs
 }
 
 func (h Hand)Score() int {
@@ -59,48 +98,57 @@ func min(a, b int) int {
 	return b
 }
 
-func main() {
+func Shuffle(gs GameState) GameState {
+	ret := clone(gs)
 	cardDeck := deck.New()
 	cardDeck2 := deck.New()
 	cardDeck3 := deck.New()
 
 	cards := deck.CombineDecks(cardDeck, cardDeck2, cardDeck3)
 	cards = deck.Shuffle(cards)
+	ret.Deck = cards
+	return ret
+}
 
-	var player, dealer Hand
+func Deal(gs GameState) GameState {
+	ret := clone(gs)
+	ret.Player = make(Hand, 0, 4)
+	ret.Dealer = make(Hand, 0, 4)
 	var card deck.Card
-
 	for i := 0; i < 2; i++ {
-		for _, hand := range []*Hand{&player, &dealer} {
-			card, cards = draw(cards)
-			*hand = append(*hand, card)
-		}
+		card, ret.Deck = draw(ret.Deck)
+		ret.Player = append(ret.Player, card)
+		card, ret.Deck = draw(ret.Deck)
+		ret.Dealer = append(ret.Dealer, card)
 	}
+	ret.State = PlayerTurn
+	return ret
+}
 
-	var input string
-	reader := bufio.NewReader(os.Stdin)
-
-	for input != "s" {
-		fmt.Println("Player's hand:", player)
-		fmt.Println("Dealer's hand", dealer.DealerString())
-		fmt.Print("What do you want to do? (s)tand or (h)old: ")
-		input, _ = reader.ReadString('\n')
-		input = input[0:len(input)-1]
-		switch input {
-		case "h":
-			card, cards = draw(cards)
-			player = append(player, card)
-		}
+func Hit(gs GameState) GameState {
+	ret := clone(gs)
+	hand := ret.CurrentPlayer()
+	var card deck.Card
+	card, ret.Deck = draw(ret.Deck)
+	*hand = append(*hand, card)
+	if hand.Score() > 21 {
+		return Stand(ret)
 	}
-	if dealer.Score() < 16 || (dealer.Score() == 17 && dealer.getLesserScore() != 17) {
-		card, cards = draw(cards)
-		dealer = append(dealer, card)
-	}
+	return ret
+}
 
-	playerScore, dealerScore := player.Score(), dealer.Score()
+func Stand(gs GameState) GameState {
+	ret := clone(gs)
+	ret.State++
+	return ret
+}
+
+func EndGame(gs GameState) GameState {
+	ret := clone(gs)
+	playerScore, dealerScore := ret.Player.Score(), ret.Dealer.Score()
 	fmt.Println("Game ending. Final hands:")
-	fmt.Println("Player's hand:", player)
-	fmt.Println("Dealer's hand:", dealer)
+	fmt.Println("Player's hand:", ret.Player)
+	fmt.Println("Dealer's hand:", ret.Dealer)
 	switch {
 	case playerScore > 21:
 		fmt.Println("Player busted.")
@@ -113,6 +161,45 @@ func main() {
 	case playerScore == dealerScore:
 		fmt.Println("Draw")
 	}
+	ret.Player = nil
+	ret.Dealer = nil
+	return ret
+}
+
+func main() {
+	var gs GameState
+	gs = Shuffle(gs)
+	gs = Deal(gs)
+
+	var input string
+	reader := bufio.NewReader(os.Stdin)
+	for gs.State == PlayerTurn {
+		fmt.Println("Player's hand:", gs.Player)
+		fmt.Println("Dealer's hand", gs.Dealer.DealerString())
+		fmt.Print("What do you want to do? (s)tand or (h)old: ")
+		input, _ = reader.ReadString('\n')
+		input = input[0:len(input)-1]
+		switch input {
+		case "h":
+			gs = Hit(gs)
+		case "s":
+			gs = Stand(gs)
+		default:
+			fmt.Println("Not a valid option: ", input)
+		}
+	}
+
+	for gs.State == DealerTurn {
+		if gs.Dealer.Score() < 16 || (gs.Dealer.Score() == 17 && gs.Dealer.getLesserScore() != 17) {
+			gs = Hit(gs)
+		} else {
+			gs = Stand(gs)
+		}
+	}
+
+	gs = EndGame(gs)
+
+
 
 
 }
